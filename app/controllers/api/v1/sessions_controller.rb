0413cb970b77cec825f5e4f2d8620d90d48c286f
lNewSkyl <atom-1121@mail.ru>
2022-01-25 14:39:02 +0300

    some code correction in post controller, devise user model setup, sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+module Api
+	module V1
+		class SessionsController < ApplicationController
+			def create
+			end
+
+			def destroy
+			end
+			
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :users
   namespace 'api' do
     namespace 'v1' do 
       resources :posts
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
